558febff65145cce215dcf2f2cb49a679937f14b
Giacomo Ciro <giacomociro02@gmail.com>
2024-07-30 14:20:07 +0200

    update index.html w/ bootstrap, rest to be done

--- script.js
+++ script.js
@@ -104,6 +104,7 @@ function updateAbout(){
 //             });
 // console.log('Timeline updated from https://giacomo-ciro.github.io/Resources/about.txt')
 };
+
 //------------------------------------------- call everything
 window.onload = function() {
   // if on the project page, update projects
@@ -119,3 +120,23 @@ window.onload = function() {
   includeFooter();
   jumpingName();
 };
+
+function toggleScrolled() {
+  const selectBody = document.querySelector('body');
+  const selectHeader = document.querySelector('#header');
+  if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+  window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+}
+
+document.addEventListener('scroll', toggleScrolled);
+window.addEventListener('load', toggleScrolled);
+
+function aosInit() {
+  AOS.init({
+    duration: 600,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  });
+}
+window.addEventListener('load', aosInit);
